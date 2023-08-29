2f8f21ae4613100bd72006580e6f167e965fb2b8
Jonathan Theron <jonathan.theron@gmx.fr>
2023-08-29 15:43:50 +0200

    added atsoins

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+      <div>Loading</div>
+    </>
+  );
+};
 
-export default loading
+export default loading;
--- constants/index.js
+++ constants/index.js
@@ -19,20 +19,28 @@ export const socials = [
 
   export const projets = [
     {
+      name: 'AT-Soins',
+      url: 'https://atsoins.fr/',
+      img: '/atsoins.png',
+      id: 1,
+      alt: 'AT-Soins',
+      backside: 'Restaurant de viande près de Lyon. Réalisé avec Next.js & contentful (CMS)',
+      techno: ["all", "next"]
+    },
+    {
       name: 'La Taverne',
       url: 'https://celebrated-blini-013655.netlify.app/',
       img: '/lataverne.jpg',
-      id: 1,
+      id: 2,
       alt: 'La taverne',
       backside: 'Restaurant de viande près de Lyon. Réalisé avec Next.js & contentful (CMS)',
       techno: ["all", "next", "contentful"]
-
     },
     {
       name: 'Covid 19 Tracker',
       url: 'https://covid-tracker-fad65.web.app/',
       img: '/covid.png',
-      id: 2,
+      id: 3,
       alt: 'Covid 19 tracker',
       backside: 'Evolution du covid 19. Réalisé avec React',
       techno: ["all", "react"]
@@ -41,7 +49,7 @@ export const socials = [
       name: 'Blog',
       url: 'https://my-blog-jojin889.vercel.app/',
       img: '/blog.png',
-      id: 3,
+      id: 4,
       alt: 'image de mon blog',
       backside: 'Mon blog. Réalisé avec Next.js & Sanity (CMS)',
       techno: ["all", "next", "sanity", "tailwind"]
@@ -50,7 +58,7 @@ export const socials = [
       name: 'Recipie',
       url: "/",
       img: '/recipie.jpg',
-      id: 3,
+      id: 5,
       alt: 'image du site en construction',
       backside: 'Appli en développement',
       techno: ["all", "next", "sanity", "tailwind"]
